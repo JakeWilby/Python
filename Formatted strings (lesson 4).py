9f6d3822941bc9ba24d44c07433f73e7f9d2bab9
Jake Wilby <130995840+JakeWilby@users.noreply.github.com>
2023-04-18 22:31:30 +0100

    Add files via upload

---
+++ Formatted strings (lesson 4).py
@@ -0,0 +1,4 @@
+first_name = 'Jake'
+last_name = 'Wilby'
+msg = f'{first_name} [{last_name}] is learning to code'
+print(msg)
---
+++ Type Conversion (lesson 3).py
@@ -0,0 +1,3 @@
+weight_lbs = input('What is your weight in lbs? ')
+weight_kgs = int(weight_lbs) * 0.453592
+print(weight_kgs)
